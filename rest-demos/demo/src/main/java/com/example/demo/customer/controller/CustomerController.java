1635990e32771320a57d94f79367695a0e26eb35
vineetsemwal <vineetsemwal82@gmail.com>
2022-08-10 11:47:40 +0530

    rest demo improved

--- rest-demos/demo/src/main/java/com/example/demo/customer/controller/CustomerController.java
+++ rest-demos/demo/src/main/java/com/example/demo/customer/controller/CustomerController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -14,6 +15,7 @@ import com.example.demo.customer.dto.AddCustomerRequest;
 import com.example.demo.customer.dto.CustomerDetails;
 import com.example.demo.customer.service.ICustomerService;
 
+@RequestMapping("/customers")
 @RestController
 public class CustomerController {
     @Autowired  
@@ -21,15 +23,15 @@ public class CustomerController {
 	
     @ResponseStatus(HttpStatus.CREATED)
     //@RequestMapping(method=RequestMethod.GET,path="/customers/byid/{id}")
-    @GetMapping("/customers/byid/{id}")
+    @GetMapping("/byid/{id}")
 	public CustomerDetails fetchCustomerById(@PathVariable long id)throws Exception {
 		CustomerDetails response=service.findCustomerDetailsById(id);
 		return response;
 	}
     
     //@RequestMapping(method=RequestMethod.POST,path="/customers/add")
-    @PostMapping("/customers/add")
-    public CustomerDetails register(AddCustomerRequest request) {
+    @PostMapping("/add")
+    public CustomerDetails register(@RequestBody AddCustomerRequest request) {
     	CustomerDetails response=service.register(request);
     	return response;
     }
